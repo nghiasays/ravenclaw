b5096a94c5e782c2366a89ccbc27d9655488addc
Nghia Nguyen <github@nngu.net>
2023-03-27 23:57:12 -0700

    Add trivy scanning

--- python/demo/main.py
+++ python/demo/main.py
@@ -3,32 +3,32 @@ import logging
 
 from fastapi import APIRouter
 
-router = APIRouter()
+app = APIRouter()
 
 logger = logging.getLogger()
 
-@router.get("/users")
+@app.get("/users")
 async def read_users():
     # Return all users
     logger.info("In read_users")
     pass
 
-@router.get("/users/{user_id}")
+@app.get("/users/{user_id}")
 async def read_user(user_id: int):
     # Return a specific user by ID
     pass
 
-@router.post("/users")
+@app.post("/users")
 async def create_user():
     # Create a new user
     pass
 
-@router.put("/users/{user_id}")
+@app.put("/users/{user_id}")
 async def update_user(user_id: int):
     # Update a specific user by ID
     pass
 
-@router.delete("/users/{user_id}")
+@app.delete("/users/{user_id}")
 async def delete_user(user_id: int):
     # Delete a specific user by ID
     pass
